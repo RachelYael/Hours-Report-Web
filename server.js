1f4510a7972c0996d6d1a09f75a0424ecf5f2c78
YaelPrat <prat.yael@gmail.com>
2021-09-19 09:35:59 +0300

    Added Node.js & Nodemon & express to the project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path= require('path');
+const fs = require('fs');
+
+app.get("/",function(req,res){
+    res.sendFile(path.resolve("./pages/WelcomePage.html"));
+})
+
+
+app.listen(3000, function(){
+    console.log("Server listen to port: 3000");
+});
