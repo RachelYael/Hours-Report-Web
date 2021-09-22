db55de121cefafee9ad090da23573ce037222b77
RachelDua <racheldua111@gmail.com>
2021-09-22 20:07:05 +0300

    design html page

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 const path= require('path');
 const fs = require('fs');
 
+app.get('/Login', (req,res) => {
+    res.sendFile(path.resolve("./pages/Login.html"));
+});
+
+app.get('/Register', (req,res) => {
+    res.sendFile(path.resolve("./pages/Register.html"));
+});
+
 app.get("/",function(req,res){
     res.sendFile(path.resolve("./pages/WelcomePage.html"));
 })
