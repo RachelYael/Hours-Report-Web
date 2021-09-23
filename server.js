d55081d994b06227d1475405bf0a264046d5474f
YaelPrat <prat.yael@gmail.com>
2021-09-23 08:00:05 +0300

    add link to Login & register

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.get('/',function(req,res){
 	res.sendFile(path.resolve('./pages/WelcomePage.html'));
 });
 
+app.get('/Login',function(req,res){
+	res.sendFile(path.resolve('./pages/Login.html'));
+});
+
+app.get('/Register',function(req,res){
+	res.sendFile(path.resolve('./pages/Register.html'));
+});
 
 app.listen(3000, function(){
 	console.log('Server listen to port: 3000');
