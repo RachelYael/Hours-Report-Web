e6f672c1eada7e0b49f16cdf06eb2a7ef5c14d40
YaelPrat <prat.yael@gmail.com>
2021-09-26 21:14:37 +0300

    i didnt change a thing but he didnt eccept that so i had to push it agin.

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const request = require('request');
+
 const express = require('express');
 const app = express();
+app.set('view engine','ejs');
 const path= require('path');
+const { readFile } = require('fs');
+const UserModel = require(path.resolve('./models/User.js'));
+mongoose.connect('mongodb://root:example@localhost:27017/',{
+		dbName:'myapp',
+		useNewUrlParser : true}).then(console.log("mongoose connected"));
+
 app.use('/assets',express.static('assets'));
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get('/',function(req,res){
 	res.sendFile(path.resolve('./pages/WelcomePage.html'));
@@ -23,6 +36,39 @@ app.get('/Register',function(req,res){
 	res.sendFile(path.resolve('./pages/Register.html'));
 });
 
-app.listen(3000, function(){
-	console.log('Server listen to port: 3000');
+app.post('/Register',async function(req,res){
+	var username = req.body.Username;
+	var password = req.body.Password;
+	var totalHours = req.body.Hours;
+	var totalMoney = req.body.Money;
+	try{
+		const user = await UserModel.findOne({username:username});
+	if(user){
+		throw new Error('This username: '+username+' allready taken!\nPlease try somthing else.');
+	}
+	const newUser = await UserModel.create({username,password,totalHours,totalMoney});
+	}
+	catch(error){
+		console.log("ERROR!\n"+error);
+		// TODO: Why this dont work???? 
+		 //readFile(path.resolve('./pages/Error.html'));
+		 //res.sendFile(path.resolve('./pages/Error.html'));
+
+		res.send("<h1>error !</h1>");
+	}
+	// TODO=> Validetion:
+	// 	   check the hours&money is numbers , >=0
+
+	
+	//TODO => idk how to take the user to the home page. Thes dosent work.....
+	//res.sendFile(path.resolve('./pages/VolunteerHomePage.html'));
+	//return res.redirect(path.resolve('./pages/VolunteerHomePage.html'));
+});
+
+app.post('/Error',function(req,res){
+	res.redirect('/Register');
+});
+const myPORT = process.env.PORT || 3000;
+app.listen(myPORT, function(){
+	console.log('Server listen to port:'+myPORT);
 });
